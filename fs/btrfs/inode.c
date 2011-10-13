0505680576742d3caef2520e107943601b6bb645
Liu Bo <liubo2009@cn.fujitsu.com>
2011-10-12 20:05:30 -0400

    Btrfs: still update inode trans stuff when size doesn't change
    Due to DIO stuff, commit 1ef30be142d2cc60e2687ef267de864cf31be995 makes btrfs
    not call btrfs_update_inode when it does not update i_disk_size, but in buffer
    write case, we need to update btrfs internal inode's trans stuff, so that the
    log code can find the inode's changes.
    
    Signed-off-by: Liu Bo <liubo2009@cn.fujitsu.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -1795,6 +1795,8 @@ static int btrfs_finish_ordered_io(struct inode *inode, u64 start, u64 end)
 	if (!ret || !test_bit(BTRFS_ORDERED_PREALLOC, &ordered_extent->flags)) {
 		ret = btrfs_update_inode(trans, root, inode);
 		BUG_ON(ret);
+	} else {
+		btrfs_set_inode_last_trans(trans, inode);
 	}
 	ret = 0;
 out:
